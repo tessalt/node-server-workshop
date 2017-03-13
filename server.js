a463c5fd65ec9f9cf5aba5acc6fd10590574deee
Tessa Thornton <tessathornton@gmail.com>
2017-03-13 19:06:40 -0400

    connect to api

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var fs = require('fs');
+var request = require('request');
 
-var re = /^\/currency\/(\w+)/;
+var re = /^\/currencies\/(\w+)/;
 
 var server = http.createServer(); // creates an eventemitter
 
@@ -20,6 +21,13 @@ function currencyForCountry(country) {
   return currencies[country] || '';
 }
 
+function getLiveValue(symbol, callback) {
+  request.get('http://api.fixer.io/latest?base=USD', function (error, response, body) {
+    var rates = JSON.parse(body).rates;
+    callback(rates[symbol]);
+  });
+}
+
 server.on('request', function (request, response) {
   request.on('error', function(err) {
     console.error(err.stack);
@@ -32,18 +40,23 @@ server.on('request', function (request, response) {
   var match = request.url.match(re);
 
   if (match) {
+
     var currency = currencyForCountry(match[1]);
-    var string = 'currency for ' + match[1] + ' is ' + currency;
-    fs.readFile('./currency.html', 'utf-8', function (error, contents) {
-      var output = contents.replace('$contents', string);
-      response.write(output, 'utf-8');
-      response.end();
+    getLiveValue(currency, function (rate) {
+      var string = 'currency for ' + match[1] + ' is ' + currency;
+      var string2 = '<br> 1 USD = ' + rate + ' ' + currency;
+      fs.readFile('./currency.html', 'utf-8', function (error, contents) {
+        var output = contents.replace('$contents', string + string2);
+        response.write(output, 'utf-8');
+        response.end();
+      });
     });
   } else {
     response.statusCode = 404;
     response.end();
   }
-
 });
 
-server.listen(8080);
+server.listen(8080, function () {
+  console.log('server is listening on port 8080');
+});
