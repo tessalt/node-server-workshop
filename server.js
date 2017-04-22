d5af77fbbca38191c5d8817c16b6dae3f8756454
Tessa Thornton <tessathornton@gmail.com>
2017-04-22 15:03:57 -0400

    complicate things

--- server.js
+++ server.js
@@ -4,56 +4,74 @@ var request = require('request');
 
 var server = http.createServer();
 
-var re = /^\/currencies\/(\w+)/;
-
 var currencies = {
   'CAN': 'CAD',
   'USA': 'USD',
   'FRA': 'EUR',
+  'SPA': 'EUR'
 }
 
-function getValue(currency, callback) {
-  request.get('http://api.fixer.io/latest?base=USD', function (error, response, body) {
-    var value = JSON.parse(body).rates[currency];
-
-    callback(value);
+function getLiveValue(symbol, callback) {
+  request.get('http://api.fixer.io/latest?base=USD', function(error, response, body) {
+    var rates = JSON.parse(body).rates;
+    callback(rates[symbol]);
   });
 }
 
-server.on('request', function(request, response) {
 
-  request.on('error', function(err) {
-    console.error(err);
+var re = /^\/currencies\/(\w+)/;
+
+server.listen(8080, function () {
+  console.log('listening on port 8080');
+});
+
+server.on('request', function (request, response) {
+
+  request.on('error', function (error) {
+    console.error(error);
   });
-
-  response.on('error', function(err) {
-    console.error(err);
+  response.on('error', function (error) {
+    console.error(error);
   });
 
   var match = request.url.match(re);
 
   if (match) {
-    var currency = currencies[match[1]] || '';
-
-
-    getValue(currency, function(value) {
-      var sentence = `The currency for ${match[1]} is ${currency}. <br> 1 USD = ${value} ${currency}`;
-
-      fs.readFile('index.html', 'utf-8', function(error, contents) {
-        response.write(contents.replace('$contents', sentence));
+    var currency = currencies[match[1].toUpperCase()];
+    if (currency) {
+      getLiveValue(currency, function (value) {
+        var output = {
+          currency: currency,
+          value: value
+        }
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify(output), 'utf-8');
         response.end();
-      });
+      })      
+    } else {
+      response.write('Not a valid country code')
+      response.end();
+    }
+  } else if (request.url === '/home') {
+     fs.readFile('index.html', function (error, contents) {
+      response.write(contents);
+      response.end();
     });
-
   } else {
-    response.write('not found');
+    response.write('Not found');
     response.statusCode = 404;
     response.end();
   }
 
-
-});
-
-server.listen(8080, function() {
-  console.log('server is listening');
+  // if (request.url === '/home') {
+  //   fs.readFile('index.html', function (error, contents) {
+  //     response.write(contents);
+  //     response.end();
+  //   });
+  // } else {
+  //   response.write('Not found');
+  //   response.statusCode = 404;
+  //   response.end();
+  // }
+ 
 });
